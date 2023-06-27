f86c54d06332196a5b7b6b7ed2c9698e57193198
tajamul86 <tajamulali86@gmail.com>
2023-06-27 06:02:39 +0530

    first commit

--- app/ar/page.js
+++ app/ar/page.js
@@ -1,8 +1,8 @@
-import Image from 'next/image'
+export default function ArHome(){
+    return(
+        <>
 
-export default function Home() {
-  return (
-    <section className="text-gray-600 body-font">
+<section className="text-gray-600 body-font">
   <div className="container px-5 py-24 mx-auto flex flex-col">
     <div className="lg:w-4/6 mx-auto">
       <div className="rounded-lg h-64 overflow-hidden">
@@ -15,7 +15,7 @@ export default function Home() {
       <div className="flex flex-col sm:flex-row mt-10">
         <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
           <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
-            {/* <svg
+            <svg
               fill="none"
               stroke="currentColor"
               strokeLinecap="round"
@@ -26,33 +26,28 @@ export default function Home() {
             >
               <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
               <circle cx={12} cy={7} r={4} />
-            </svg> */}
+            </svg>
           </div>
           <div className="flex flex-col items-center text-center justify-center">
             <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">
-              Phoebe Caulfield
+            تعال إلى
             </h2>
             <div className="w-12 h-1 bg-green-500 rounded mt-2 mb-4" />
             <p className="text-base">
-              Raclette knausgaard hella meggs normcore williamsburg enamel pin
-              sartorial venmo tbh hot chicken gentrify portland.
+            Raclette Knausgaard دبوس المينا هيلا ميجز نوركوركور ويليامسبيرج
+              سارتوريال فينمو دجاج حار جنتريفي بورتلاند.
+
             </p>
           </div>
         </div>
-        <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+        <div className="sm:w-2/3 sm:pr-8 sm:py-8 sm:border-r border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-right">
           <p className="leading-relaxed text-lg mb-4">
-            Meggings portland fingerstache lyft, post-ironic fixie man bun banh
-            mi umami everyday carry hexagon locavore direct trade art party.
-            Locavore small batch listicle gastropub farm-to-table lumbersexual
-            salvia messenger bag. Coloring book flannel truffaut craft beer
-            drinking vinegar sartorial, disrupt fashion axe normcore meh
-            butcher. Portland 90's scenester vexillologist forage post-ironic
-            asymmetrical, chartreuse disrupt butcher paleo intelligentsia pabst
-            before they sold out four loko. 3 wolf moon brooklyn.
+          Meggings بورتلاند Fingerstache lyft ، رجل مثبت ما بعد السخرية بن بانه مي أومامي كل يوم يحمل سداسي الزوايا locavore مباشرة حفلة فنية تجارية. حقيبة لوكافور صغيرة الحجم من المزرعة من المزرعة إلى المائدة. كتاب تلوين الفانيلا truffaut الحرفية البيرة شرب الخل sartorial ، تعطيل الأزياء الفأس نوركوركور مه جزار. قام عالم vexillologist في بورتلاند 90 في البحث عن الأعلاف بعد السخرية غير المتكافئة ، وتعطل الشارترايز جزار النخبة المثقفة قبل أن يبيعوا أربعة من loko. 3 وولف مون بروكلين.
+
           </p>
           <a className="text-green-500 inline-flex items-center">
             Learn More
-            {/* <svg
+            <svg
               fill="none"
               stroke="currentColor"
               strokeLinecap="round"
@@ -60,9 +55,9 @@ export default function Home() {
               strokeWidth={2}
               className="w-4 h-4 ml-2"
               viewBox="0 0 24 24"
-            > 
+            >
               <path d="M5 12h14M12 5l7 7-7 7" />
-          </svg>*/}
+            </svg>
           </a>
         </div>
       </div>
@@ -70,6 +65,6 @@ export default function Home() {
   </div>
 </section>
 
-
-  )
+        </>
+    )
 }
